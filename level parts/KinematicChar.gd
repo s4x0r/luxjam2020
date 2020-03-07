e945e35a70beeaf669cef0887e91151c8c4353d8
denneisk <denneisk@protonmail.com>
2020-03-07 09:40:47 -0500

    Improved velocity and sliding.

--- level parts/KinematicChar.gd
+++ level parts/KinematicChar.gd
@@ -3,8 +3,8 @@ extends KinematicBody2D
 signal damaged
 
 const GRAVITY = 200.0
-const WALK_SPEED = 2
-const FRICTION = .66 * WALK_SPEED
+const WALK_SPEED = 3.5
+const FRICTION = 1.05	#Friction mechanism may be improved, see line 38
 var collision
 var velocity = Vector2()
 
@@ -14,29 +14,34 @@ func _physics_process(delta):
 	var ud = Input.is_action_pressed("ui_down") as int - Input.is_action_pressed("ui_up") as int
 	
 	if lr:	#If lr (left/right) is nonzero
-		if lr==1:	#right - left == 1 if left is not pressed
+		if lr == 1:	#right - left == 1 if left is not pressed
 			$AnimatedSprite.play('right')
 		else:	#Else it would be left
 			$AnimatedSprite.play('left')
-		velocity.y = velocity.y / FRICTION
-		velocity.x = WALK_SPEED * lr
+		velocity.x += WALK_SPEED * lr * delta
 		
 	if ud:	#If ud (Up/Down) is nonzero
-		if ud==1:	#down - up == 1 if up is not pressed
+		if ud == 1:	#down - up == 1 if up is not pressed
 			$AnimatedSprite.play('down')
 		else:	#Else it would be up
 			$AnimatedSprite.play('up')
-		velocity.x = velocity.x / FRICTION
-		velocity.y = WALK_SPEED * ud
+		velocity.y += WALK_SPEED * ud * delta
 	
 	if !(lr|ud):	
 		$AnimatedSprite.stop()
-		velocity = Vector2(velocity.x/FRICTION,velocity.y/FRICTION)
+	
+	if abs(velocity.x)>WALK_SPEED:
+		velocity.x -= WALK_SPEED / velocity.x
+	if abs(velocity.y)>WALK_SPEED:
+		velocity.y -= WALK_SPEED / velocity.y
+		
+	velocity = velocity/FRICTION	#Player slides, how cool.
 	
 	collision = move_and_collide(velocity)
 	if collision != null:
 		if not collision.collider.get_parent().get("moveable")==null:
 			collision.collider.get_parent().position += velocity*8
-		move_and_collide(-velocity*8)
+		velocity = -velocity*2	#Player bounces in opposite direction
+		#move_and_collide(velocity)
 		$AudioStreamPlayer.play()
 		emit_signal("damaged", self)
