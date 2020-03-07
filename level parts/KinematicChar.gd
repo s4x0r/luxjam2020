4f56f1b823b31b89cdb1eca9c6dad8c689c5cc1d
denneisk <denneisk@protonmail.com>
2020-03-07 09:02:25 -0500

    Revised movement for KinematicChar to emulate char.gd.

--- level parts/KinematicChar.gd
+++ level parts/KinematicChar.gd
@@ -3,32 +3,35 @@ extends KinematicBody2D
 signal damaged
 
 const GRAVITY = 200.0
-const WALK_SPEED = 1
+const WALK_SPEED = 2
+const FRICTION = .66 * WALK_SPEED
 var collision
 var velocity = Vector2()
 
 func _physics_process(delta):
 	collision = null
+	var lr = Input.is_action_pressed("ui_right") as int - Input.is_action_pressed("ui_left") as int
+	var ud = Input.is_action_pressed("ui_down") as int - Input.is_action_pressed("ui_up") as int
 	
-	if Input.is_action_pressed("ui_left"):
-		$AnimatedSprite.play('left')
-		velocity.y = 0
-		velocity.x = -WALK_SPEED
-	elif Input.is_action_pressed("ui_right"):
-		$AnimatedSprite.play('right')
-		velocity.y = 0
-		velocity.x = WALK_SPEED
-	elif Input.is_action_pressed("ui_up"):
-		$AnimatedSprite.play('up')
-		velocity.x = 0
-		velocity.y = -WALK_SPEED
-	elif Input.is_action_pressed("ui_down"):
-		$AnimatedSprite.play('down')
-		velocity.x = 0
-		velocity.y = WALK_SPEED
-	else:
+	if lr:	#If lr (left/right) is nonzero
+		if lr==1:	#right - left == 1 if left is not pressed
+			$AnimatedSprite.play('right')
+		else:	#Else it would be left
+			$AnimatedSprite.play('left')
+		velocity.y = velocity.y / FRICTION
+		velocity.x = WALK_SPEED * lr
+		
+	if ud:	#If ud (Up/Down) is nonzero
+		if ud==1:	#down - up == 1 if up is not pressed
+			$AnimatedSprite.play('down')
+		else:	#Else it would be up
+			$AnimatedSprite.play('up')
+		velocity.x = velocity.x / FRICTION
+		velocity.y = WALK_SPEED * ud
+	
+	if !(lr|ud):	
 		$AnimatedSprite.stop()
-		velocity = Vector2(0,0)
+		velocity = Vector2(velocity.x/FRICTION,velocity.y/FRICTION)
 	
 	collision = move_and_collide(velocity)
 	if collision != null:
